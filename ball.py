031e122cea6515789c788d6dda54e5779ff8cbf8
Thomaz, Lucian (ESI) <lucian.thomaz@adp.com>
2023-08-12 19:54:35 -0300

    Detect collision with wall and bounce. close #5

--- ball.py
+++ ball.py
@@ -8,25 +8,15 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
 
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    def go_up(self):
-        new_y = self.ycor() + 20
-        self.goto(self.xcor(), new_y)
 
-    def go_down(self):
-        new_y = self.ycor() - 20
-        self.goto(self.xcor(), new_y)
-
-    def go_left(self):
-        new_x = self.xcor() - 20
-        self.goto(new_x, self.ycor())
-
-    def go_right(self):
-        new_x = self.ycor() - 20
-        self.goto(new_x, self.ycor())
+    def bounce(self):
+        self.y_move *= -1
